be9fb142fc74497c6adf765bf502e08d17941b31
bochinche <justshiat@gmail.com>
2023-01-02 00:20:47 +0100

    new pics

--- images/process_images.sh
+++ images/process_images.sh
@@ -3,8 +3,9 @@ FILES="*.jpg"
 for i in $FILES
 do
 	echo "Prcoessing image $i ..."
-	/usr/bin/convert $i -quality 75 ./fulls/$i
-	/usr/bin/convert -thumbnail 500 $i ./thumbs/$i
-	rm $i 
+	#/usr/bin/convert $i -quality 75 ./fulls/$i
+	/usr/bin/convert $i -define jpeg:extent=500kb ./fulls/$i
+	/usr/bin/convert $i -thumbnail 500 ./thumbs/$i
+	mv $i ./originals/$i  
 	#/usr/bin/convert -thumbnail 500 $i ./thumbs/$i && mv $i ./fulls/$i
 done
